b0cccceb7ca63c2e207f70d89e79c806aeb67a01
Richard <legalize@xmission.com>
2016-08-02 23:35:41 -0600

    Combine test cases

--- iterator.cpp
+++ iterator.cpp
@@ -3,21 +3,12 @@
 
 // In each test, replace XXX with syntax that passes the test.
 
-BOOST_AUTO_TEST_CASE(beginning_of_an_array)
+BOOST_AUTO_TEST_CASE(array_iterators)
 {
 	int values[5]{ 1, 2, 3, 4, 5 };
+
 	BOOST_REQUIRE_EQUAL(&values[xxx], std::begin(values));
-}
-
-BOOST_AUTO_TEST_CASE(ending_of_an_array)
-{
-	int values[5]{ 1, 2, 3, 4, 5 };
 	BOOST_REQUIRE_EQUAL(&values[xxx], std::end(values));
-}
-
-BOOST_AUTO_TEST_CASE(type_of_array_begin_iterator)
-{
-	int values[5]{ 1, 2, 3, 4, 5 };
 	BOOST_REQUIRE((std::is_same<XXX, decltype(std::begin(values))>::value));
 	BOOST_REQUIRE((std::is_same<XXX, decltype(std::cbegin(values))>::value));
 }
@@ -26,6 +17,7 @@ BOOST_AUTO_TEST_CASE(iterator_traits_of_array_iterators)
 {
 	int values[]{ 1, 2, 3, 4, 5 };
 	auto start = std::begin(values);
+
 	BOOST_REQUIRE((std::is_same<XXX, std::iterator_traits<decltype(start)>::value_type>::value));
 	BOOST_REQUIRE((std::is_same<XXX, std::iterator_traits<decltype(start)>::difference_type>::value));
 	BOOST_REQUIRE((std::is_same<XXX, std::iterator_traits<decltype(start)>::pointer>::value));
