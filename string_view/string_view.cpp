e450e74aeeac6f353d2e781978a81fb1a5da6315
Richard <legalize@xmission.com>
2018-01-09 00:32:59 -0700

    Add string_view test cases and remove unrelated cases from string_view

--- string_view/string_view.cpp
+++ string_view/string_view.cpp
@@ -1,49 +1,31 @@
 #include "string_view.h"
 
-BOOST_AUTO_TEST_CASE(character_arrays)
+BOOST_AUTO_TEST_CASE(string_view_variants)
 {
-    char s[]{"Hello, world!"};
-
-    BOOST_REQUIRE_EQUAL(xxx, std::begin(s));
-    BOOST_REQUIRE_EQUAL(xxx, std::end(s));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::basic_string_view<char>>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::wstring_view>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::u16string_view>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::u32string_view>));
 }
 
-BOOST_AUTO_TEST_CASE(std_string_variants)
+BOOST_AUTO_TEST_CASE(string_view_related_types)
 {
-    BOOST_REQUIRE((std::is_same_v<std::string, std::basic_string<XXX>>));
-    BOOST_REQUIRE((std::is_same_v<std::wstring, std::basic_string<XXX>>));
-    BOOST_REQUIRE((std::is_same_v<std::u16string, std::basic_string<XXX>>));
-    BOOST_REQUIRE((std::is_same_v<std::u32string, std::basic_string<XXX>>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::traits_type>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::value_type>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::pointer>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::const_pointer>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::reference>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::const_reference>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::size_type>));
+    BOOST_REQUIRE((std::is_same_v<XXX, std::string_view::difference_type>));
 }
 
-BOOST_AUTO_TEST_CASE(std_string_related_types)
+BOOST_AUTO_TEST_CASE(string_view_constructors)
 {
-    BOOST_REQUIRE((std::is_same_v<std::string::traits_type, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::value_type, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::reference, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::const_reference, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::reverse_iterator, std::reverse_iterator<XXX>>));
-    BOOST_REQUIRE((std::is_same_v<std::string::const_reverse_iterator, std::reverse_iterator<XXX>>));
-}
+    constexpr std::string_view s;
+    BOOST_REQUIRE_EQUAL(xxx, s.empty());
 
-BOOST_AUTO_TEST_CASE(std_string_accessors)
-{
-    std::string s{"Hello, world!"};
-
-    BOOST_REQUIRE_EQUAL(xxx, s[0]);
-    BOOST_REQUIRE_EQUAL('w', s.at(xxx));
-    BOOST_REQUIRE_THROW(s.at(1000), XXX);
-    BOOST_REQUIRE_EQUAL(xxx, s.front());
-    BOOST_REQUIRE_EQUAL(xxx, s.back());
-    BOOST_REQUIRE((std::is_same_v<XXX, decltype(s.data())>));
-    BOOST_REQUIRE((std::is_same_v<XXX, decltype(s.c_str())>));
-}
-
-BOOST_AUTO_TEST_CASE(std_string_allocators)
-{
-    BOOST_REQUIRE((std::is_same_v<std::string::allocator_type, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::size_type, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::difference_type, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::pointer, XXX>));
-    BOOST_REQUIRE((std::is_same_v<std::string::const_pointer, XXX>));
+    constexpr std::string_view s2{"Hello, world!", 5};
+    BOOST_REQUIRE_EQUAL(xxx, s.length());
 }
