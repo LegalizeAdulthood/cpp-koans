42635901fc8c7514dc3129cdd4dd0c4d3f54f427
Richard <legalize@xmission.com>
2018-01-09 01:53:20 -0500

    Add test cases for std::string

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -18,6 +18,50 @@ BOOST_AUTO_TEST_CASE(std_string_related_types)
     BOOST_REQUIRE((std::is_same_v<std::string::const_reverse_iterator, std::reverse_iterator<XXX>>));
 }
 
+BOOST_AUTO_TEST_CASE(std_string_constructors)
+{
+    std::string const s1;
+    BOOST_REQUIRE_EQUAL(xxx, s1.length());
+
+    std::string const s2{5, 'E'};
+    BOOST_REQUIRE_EQUAL(xxx, s2.length());
+
+    std::string const s3{s2};
+    BOOST_REQUIRE_EQUAL(xxx, s3.length());
+
+    std::string const s4{s2, 1};
+    BOOST_REQUIRE_EQUAL(xxx, s4.length());
+
+    std::string const hw{"Hello, world!"};
+    std::string const s5{hw, 1, 4};
+    BOOST_REQUIRE_EQUAL(xxx, s5);
+
+    std::string const s6{"Hello, world!"};
+    BOOST_REQUIRE_EQUAL(xxx, s6.length());
+
+    std::string const s7{"Hello, world!", 5};
+    BOOST_REQUIRE_EQUAL(xxx, s7);
+
+    std::string const s8{s7.begin(), s7.begin() + 1};
+    BOOST_REQUIRE_EQUAL(xxx, s8);
+
+    std::string const s9{'H', 'e', 'l', 'l', 'o'};
+    BOOST_REQUIRE_EQUAL(xxx, s9);
+}
+
+BOOST_AUTO_TEST_CASE(std_string_literals)
+{
+    using namespace std::literals::string_literals;
+    auto s1{"Hello, world!"s};
+    auto s2{L"Hello, world!"s};
+    auto s3{u"Hello, world!"s};
+    auto s4{U"Hello, world!"s};
+    BOOST_REQUIRE((std::is_same_v<decltype(s1), XXX>));
+    BOOST_REQUIRE((std::is_same_v<decltype(s2), XXX>));
+    BOOST_REQUIRE((std::is_same_v<decltype(s3), XXX>));
+    BOOST_REQUIRE((std::is_same_v<decltype(s4), XXX>));
+}
+
 BOOST_AUTO_TEST_CASE(std_string_accessors)
 {
     std::string s{"Hello, world!"};
@@ -29,6 +73,10 @@ BOOST_AUTO_TEST_CASE(std_string_accessors)
     BOOST_REQUIRE_EQUAL(xxx, s.back());
     BOOST_REQUIRE((std::is_same_v<decltype(s.data()), XXX>));
     BOOST_REQUIRE((std::is_same_v<decltype(s.c_str()), XXX>));
+}
+
+BOOST_AUTO_TEST_CASE(string_iterators)
+{
 }
 
 BOOST_AUTO_TEST_CASE(std_string_allocators)
