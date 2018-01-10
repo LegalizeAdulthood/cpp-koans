3d68ee5e1c95c8a4a6c7300b403fb1573b967ca0
Richard <legalize@xmission.com>
2018-01-09 22:48:59 -0700

    Move using namespace std::string_literals to file scope. Add test case for std::string::swap

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -1,8 +1,9 @@
 #include "strings.h"
 
+using namespace std::string_literals;
+
 BOOST_AUTO_TEST_CASE(std_string_literals)
 {
-    using namespace std::string_literals;
     auto s1{"Hello, world!"s};
     auto s2{L"Hello, world!"s};
     auto s3{u"Hello, world!"s};
@@ -119,8 +120,6 @@ BOOST_AUTO_TEST_CASE(std_string_clear)
 
 BOOST_AUTO_TEST_CASE(std_string_insert)
 {
-    using namespace std::string_literals;
-
     std::string s2{"Bob"};
     auto res1 = s2.insert(1, "illy B");
     BOOST_REQUIRE((std::is_same_v<decltype(res1), XXX>));
@@ -160,7 +159,6 @@ BOOST_AUTO_TEST_CASE(std_string_insert)
 
 BOOST_AUTO_TEST_CASE(std_string_erase)
 {
-    using namespace std::string_literals;
     std::string s1{"Hello, world!"};
     s1.erase(5);
     BOOST_REQUIRE_EQUAL(xxx, s1);
@@ -181,7 +179,6 @@ BOOST_AUTO_TEST_CASE(std_string_erase)
 
 BOOST_AUTO_TEST_CASE(std_string_push_back)
 {
-    using namespace std::string_literals;
     std::string s{"<"};
     char c{xxx};
     s.push_back(c);
@@ -197,7 +194,6 @@ BOOST_AUTO_TEST_CASE(std_string_pop_back)
 
 BOOST_AUTO_TEST_CASE(std_string_append)
 {
-    using namespace std::string_literals;
     std::string s1{"<["};
     auto res1 = s1.append(xxx, 2);
     BOOST_REQUIRE((std::is_same_v<decltype(res1), XXX>));
@@ -232,7 +228,6 @@ BOOST_AUTO_TEST_CASE(std_string_append)
 
 BOOST_AUTO_TEST_CASE(std_string_concatenation_operator)
 {
-    using namespace std::string_literals;
     std::string s1{"One"};
     std::string s2{s1 + static_cast<char>(xxx)};
     BOOST_REQUIRE_EQUAL("One!"s, s2);
@@ -252,7 +247,6 @@ BOOST_AUTO_TEST_CASE(std_string_concatenation_operator)
 
 BOOST_AUTO_TEST_CASE(std_string_comparison_operators)
 {
-    using namespace std::string_literals;
     const std::string alpha{"Alpha"};
     const std::string beta{"Beta"};
     bool lt = static_cast<std::string>(xxx) < beta;
@@ -274,7 +268,6 @@ BOOST_AUTO_TEST_CASE(std_string_comparison_operators)
 
 BOOST_AUTO_TEST_CASE(std_string_compare)
 {
-    using namespace std::string_literals;
     const std::string s1{"Alphabetical"};
     const std::string s2{"Alphanumerical"};
     auto res1 = s1.compare(s2);
@@ -299,7 +292,6 @@ BOOST_AUTO_TEST_CASE(std_string_compare)
 
 BOOST_AUTO_TEST_CASE(std_string_replace)
 {
-    using namespace std::string_literals;
     std::string s{"Canonical"};
     auto res = s.replace(xxx, xxx, "Med"s);
     BOOST_REQUIRE((std::is_same_v<decltype(res), XXX>));
@@ -349,7 +341,6 @@ BOOST_AUTO_TEST_CASE(std_string_replace)
 
 BOOST_AUTO_TEST_CASE(std_string_substr)
 {
-    using namespace std::string_literals;
     std::string s{"Holly Jolly Folly"};
     BOOST_REQUIRE_EQUAL(xxx, s.substr());
 
@@ -360,7 +351,6 @@ BOOST_AUTO_TEST_CASE(std_string_substr)
 
 BOOST_AUTO_TEST_CASE(std_string_copy)
 {
-    using namespace std::string_literals;
     char dest[5];
     std::string s{"Holly Jolly Folly"};
     auto res = s.copy(dest, xxx);
@@ -385,7 +375,6 @@ BOOST_AUTO_TEST_CASE(std_string_copy)
 
 BOOST_AUTO_TEST_CASE(std_string_resize)
 {
-    using namespace std::string_literals;
     std::string s;
     s.resize(xxx);
     BOOST_REQUIRE_EQUAL("\0\0\0"s, s);
@@ -396,6 +385,11 @@ BOOST_AUTO_TEST_CASE(std_string_resize)
 
 BOOST_AUTO_TEST_CASE(std_string_swap)
 {
+    std::string s1{"Laurel"};
+    std::string s2{"Hardy"};
+    s1.swap(s2);
+    BOOST_REQUIRE_EQUAL("xxx"s, s1);
+    BOOST_REQUIRE_EQUAL("xxx"s, s2);
 }
 
 BOOST_AUTO_TEST_CASE(std_string_find)
